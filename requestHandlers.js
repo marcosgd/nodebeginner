6975c7824ff81619856c8e9cfc13601cbc4c22d8
marcosgd <marcosgonzalezdiaz@gmail.com>
2016-06-29 16:00:12 +0200

    2.0.0
    2.0.0 operaciones bloqueantes

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,6 +1,14 @@
 function iniciar() {
   console.log("Manipulador de petición 'iniciar' ha sido llamado.");
-  return "Hola Iniciar";
+  function sleep(milliSeconds) {
+   // obten la hora actual
+   var startTime = new Date().getTime();
+   // atasca la cpu
+   while (new Date().getTime() < startTime + milliSeconds);
+  }
+
+  sleep(10000);
+    return "Hola Iniciar";
 }
 
 function subir() {
